8d40ce19361ef51559fa5c7992b61d7a6cb7090f
vishy08 <vishyk@live.unc.edu>
2022-04-25 18:12:45 -0400

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -2,10 +2,11 @@ import { coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs";
 import minimist from "minimist";
 const myArg = (minimist)(process.argv.slice(2))
 
-var calls = myArg.calls || -1;
-if (calls==undefined || (calls!="heads" && calls!="tails")) {
+var calls = myArg.calls;
+if (calls==undefined) {
     console.log("Error: no input.")
-    console.log("Usage: node guess-flip --call=[heads|tails]")
-} else {
+} else if (calls!="heads" && calls!="tails") {
+        console.log("Usage: node guess-flip --call=[heads|tails]")
+    } else {
     console.log(flipACoin(calls));
 }
