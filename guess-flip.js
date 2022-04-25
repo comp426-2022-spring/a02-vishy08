6428169ade0865aad6dac6db4821deda595cd743
vishy08 <vishyk@live.unc.edu>
2022-04-25 18:14:33 -0400

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -3,10 +3,9 @@ import minimist from "minimist";
 const myArg = (minimist)(process.argv.slice(2))
 
 var calls = myArg.calls;
-if (calls==undefined) {
+if (calls==undefined || (calls!="heads" && calls!="tails")) {
     console.log("Error: no input.")
-} else if (calls!="heads" && calls!="tails") {
-        console.log("Usage: node guess-flip --call=[heads|tails]")
+    console.log("Usage: node guess-flip --call=[heads|tails]")
     } else {
     console.log(flipACoin(calls));
 }
